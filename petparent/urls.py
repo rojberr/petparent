6cb35e7ca44f187af35f0ac41e9833d47349ba11
Rojberr <rojberr@outlook.com>
2022-10-22 23:26:19 +0200

    feat(view): Add /healthcheck returning HTTP 200

--- petparent/urls.py
+++ petparent/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from petparent import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthcheck/', views.healthcheck),
 ]
