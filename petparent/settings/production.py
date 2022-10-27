3e0d871cf39c15727d77d855752b9f0a73748502
Rojberr <rojberr@outlook.com>
2022-10-27 11:03:52 +0200

    config(settings): Create ./settings with dev and prod settings inheriting from basic.py

---
+++ petparent/settings/production.py
@@ -0,0 +1,18 @@
+from .basic import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
