b8735f7d627c9a43011de3e710620bd031644567
rojberr <rojberr@outlook.com>
2022-12-04 12:34:32 +0100

    feat(web): Restore default Django homepage for DEBUG

--- petparent/urls.py
+++ petparent/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import debug
 
 from petparent import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('healthcheck/', views.healthcheck),
+    path('', debug.default_urlconf),
 ]
